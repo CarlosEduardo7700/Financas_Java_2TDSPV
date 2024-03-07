56a016cc5d8554f33792cd958e4b0f57593df7dd
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-03-06 22:45:20 -0300

    Usando JPA e inserindo dados no banco.

--- src/main/java/br/com/fiap/aula03/controllers/InvestimentoController.java
+++ src/main/java/br/com/fiap/aula03/controllers/InvestimentoController.java
@@ -3,24 +3,35 @@ package br.com.fiap.aula03.controllers;
 import br.com.fiap.aula03.dto.CadastroInvestimentosDto;
 import br.com.fiap.aula03.model.CategoriaInvestimento;
 import br.com.fiap.aula03.model.Investimento;
+import br.com.fiap.aula03.repositories.InvestimentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("investimentos")
 public class InvestimentoController {
 
+    @Autowired
+    private InvestimentoRepository repository;
+
     @GetMapping
     public Investimento get(){
+        List<Investimento> investimentos = repository.findAll();
         return new Investimento(1l, "CDI", CategoriaInvestimento.RENDA_FIXA, 1000.0);
     }
 
     @PostMapping
-    public ResponseEntity<CadastroInvestimentosDto> post(@RequestBody CadastroInvestimentosDto investimento, UriComponentsBuilder uriBuilder) {
-        System.out.println(investimento.getNome());
-        var uri = uriBuilder.path("/investimento/{id}").buildAndExpand(1).toUri();
-        return ResponseEntity.created(uri).body(investimento);
+    @Transactional
+    public ResponseEntity<CadastroInvestimentosDto> post(@RequestBody CadastroInvestimentosDto investimentoDto, UriComponentsBuilder uriBuilder) {
+        Investimento investimento = new Investimento(investimentoDto);
+        repository.save(investimento);
+        var uri = uriBuilder.path("/investimento/{id}").buildAndExpand(investimento.getId()).toUri();
+        return ResponseEntity.created(uri).body(investimentoDto);
     }
 
 }
--- src/main/java/br/com/fiap/aula03/model/Investimento.java
+++ src/main/java/br/com/fiap/aula03/model/Investimento.java
@@ -1,15 +1,28 @@
 package br.com.fiap.aula03.model;
 
+import br.com.fiap.aula03.dto.CadastroInvestimentosDto;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "investimentos")
 @Getter @Setter
 @AllArgsConstructor @NoArgsConstructor
 public class Investimento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    @Enumerated
     private CategoriaInvestimento categoria;
     private Double valor;
+
+    public Investimento(CadastroInvestimentosDto dto) {
+        this.nome = dto.getNome();
+        this.categoria = dto.getCategoria();
+        this.valor = dto.getValor();
+    }
 }
